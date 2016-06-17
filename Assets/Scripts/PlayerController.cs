8b39caa8d347f5d7c457029d5a13c35c35afb586
Cesar Ramirez <Cesar Ramirez>
2016-06-17 13:49:46 -0700

    Adding controller support for player prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,9 +23,34 @@ public class PlayerController : NetworkBehaviour {
         {
             return;
         }
-        float dx = InputManager.GetAxis(InputManager.CONTROLLER_ANALOG.STICK_RIGHT_X);
-        float dz = InputManager.GetAxis(InputManager.CONTROLLER_ANALOG.STICK_LEFT_Y);
 
-        rigidBody.MovePosition(new Vector3(transform.position.x + dx, transform.position.y, transform.position.z + dz));
+        float dStrafe = InputManager.GetAxis(InputManager.CONTROLLER_ANALOG.STICK_LEFT_X);
+        float dForward = InputManager.GetAxis(InputManager.CONTROLLER_ANALOG.STICK_LEFT_Y);
+        float dElevate = InputManager.GetAxis(InputManager.CONTROLLER_ANALOG.STICK_RIGHT_Y);
+        float dRotate = InputManager.GetAxis(InputManager.CONTROLLER_ANALOG.STICK_RIGHT_X) * 5;
+
+        Vector2 leftStickVector = new Vector2(dForward, dStrafe);
+        Vector2 rightStickVector = new Vector2(dElevate, dRotate);
+
+        if (rightStickVector.magnitude >= 0.2)
+        {
+            Quaternion newRotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, dRotate, 0));
+            rigidBody.MoveRotation(newRotation);
+        }
+        else
+        {
+            dElevate = 0;
+        }
+
+        if (leftStickVector.magnitude < 0.2)
+        {
+            dStrafe = 0;
+            dForward = 0;
+        }
+        if (dStrafe != 0 || dForward != 0 || dElevate != 0)
+        {
+            Vector3 dVector = new Vector3(dStrafe, dElevate, dForward) / 10;
+            rigidBody.MovePosition(transform.position + transform.TransformDirection(dVector));
+        }
     }
 }
