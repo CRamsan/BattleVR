4b6dfd16c91dbfff6961f76cf296fe73f0a302cd
Cesar Ramirez <me@cramsan.com>
2016-07-10 23:44:20 -0700

    Adding weapons

---
+++ Assets/Scripts/SkyboxManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxManager : MonoBehaviour {
+
+    public Material[] skyBoxList;
+    private int selectedIndex;
+    private Light enviromentLight;
+
+    void Start()
+    {
+        enviromentLight = (Light)(GameObject.Find("DirectionalLight").GetComponent<Light>());
+        setSkyInbox(Random.Range(0, skyBoxList.Length));
+    }
+
+    public void setSkyInbox(int index)
+    {
+        selectedIndex = index;
+        RenderSettings.skybox = skyBoxList[selectedIndex];
+        Vector3 lightDirection = getDirectionalLightOrientation(selectedIndex);
+        enviromentLight.transform.rotation = Quaternion.Euler(lightDirection.x, lightDirection.y, lightDirection.z);
+    }
+
+    private Vector3 getDirectionalLightOrientation(int index)
+    {
+        switch (index)
+        {
+            default:
+                return Vector3.zero;
+        }
+    }
+}
